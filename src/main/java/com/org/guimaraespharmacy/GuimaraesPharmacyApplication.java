49190704b05b02cd64843a8a1059e1d769dfbac8
Ryan <ryanfelipe.rr@gmail.com>
2024-03-14 00:09:37 -0300

    Structure initial of project

---
+++ src/main/java/com/org/guimaraespharmacy/GuimaraesPharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.org.guimaraespharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuimaraesPharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuimaraesPharmacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/guimaraespharmacy/GuimaraesPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.guimaraespharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuimaraesPharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
